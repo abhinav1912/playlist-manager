29a41ce5a1549d6ac3979d8d529f17b926e13aac
abhinav1912 <abhinavmathur1912@gmail.com>
2026-04-13 22:48:01 +0530

    wip: added macOS sample app

---
+++ PlaylistSampleApp/PlaylistSampleAppTests/PlaylistSampleAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlaylistSampleAppTests.swift
+//  PlaylistSampleAppTests
+//
+//  Created by Abhinav Mathur on 13/04/26.
+//
+
+import Testing
+@testable import PlaylistSampleApp
+
+struct PlaylistSampleAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
