9c9b3d97e77d91baef973ef0de7853ccd8370fcc
abhinav1912 <abhinavmathur1912@gmail.com>
2026-04-13 18:45:25 +0530

    wip: path validity checks

--- PlaylistManager/Sources/PlaylistManager/PlaylistManager.swift
+++ PlaylistManager/Sources/PlaylistManager/PlaylistManager.swift
@@ -1,23 +1,36 @@
 import Foundation
 
 class PlaylistManager {
+    let fileManager: FileManager = .default
+
     func parse(path: URL) async throws -> Playlist {
-        guard checkSupport(for: path.pathExtension) else {
+        try checkPathValidityAndPermissions(for: path)
+
+        guard let format = SupportedFormats(rawValue: path.pathExtension) else {
             throw ParsingError.unsupportedPlaylistFormat
         }
-        throw ParsingError.unsupportedPlaylistFormat
-    }
 
-    func checkSupport(for format: String) -> Bool {
-        SupportedFormats(rawValue: format) != nil
+        // TODO: Implement parsing
+        throw ParsingError.unsupportedPlaylistFormat
     }
 
     enum ParsingError: Error {
         case invalidPath
+        case insufficientPermissions
         case unsupportedPlaylistFormat
     }
 
     enum SupportedFormats: String, CaseIterable {
         case m3u
+    }
+
+    private func checkPathValidityAndPermissions(for path: URL) throws {
+        guard fileManager.fileExists(atPath: path.absoluteString) else {
+            throw ParsingError.invalidPath
+        }
+
+        guard fileManager.isReadableFile(atPath: path.absoluteString) else {
+            throw ParsingError.insufficientPermissions
+        }
     }
 }
